ac36df526054440de4567c52dac56d0115b82dd3
Pablo <bruli_pablo@hotmail.com>
2021-12-14 17:25:01 -0300

    [Feature Pablo - Task:Criando calculadora básica com javascript]

---
+++ script.js
@@ -0,0 +1,115 @@
+
+// var vOuF = false;
+// console.log(typeof(vOuF));
+
+
+// var numeroQualquer =1;
+// console.log(typeof(numeroQualquer));
+
+// var nome = "pablo";
+// console.log(typeof(nome));
+
+// var escoboGlobal = "global"
+// console.log(escoboGlobal)
+
+// function escopoLocal(){
+//     let escopoLocalInterno = "local"
+//     console.log(escopoLocalInterno)
+// }
+
+// escopoLocal()
+
+// let array = ["string",1,true]
+// console.log(array)
+
+// let array = ["string",1,true, ["array1"], ["array2"]];
+// console.log(array[0])
+
+// var jogador1 = 0;
+// var jogador2 = 0;
+// var placar;
+
+
+
+// if (jogador1 > 0 && jogador2 ==0) {
+//     console.log("Jogador 1 marcou ponto");
+//     placar = jogador1 > jogador2;
+
+// } else if (jogador2 > 0 && jogador1 == 0){
+//     console.log("Jogador 2 marcou ponto");
+//     placar = jogador2 > jogador1;
+// } else {
+//     console.log("Ninguem marcou ponto");
+// }
+
+// switch (placar) {
+//     case placar = jogador1 >jogador2:
+//         console.log("jogador 1 ganhou")
+//         break;
+//         case 
+// }
+
+
+// let array = ['valor1', 'valor2','valor3','valor4','valor5',];
+
+// let object = {propriedade1: 'valor1', propriedade2: 'valor2',propriedade3: 'valor3',
+
+// }
+
+// for(let indice = 0; indice < array.length; indice ++){
+//     console.log(indice);
+// }
+
+// for(let i in array) {
+//     console.log(i);
+// }
+
+// for(i in object) {
+// console.log(i);
+// }
+
+// for(i of array){
+//     console.log(i);
+// }
+// for(i of object.propriedade1){
+//     console.log(i);
+// }
+
+// var a = 0;
+
+// while(a<10){
+//     a++;
+//     console.log(a);
+// }
+
+// do {
+//     a++;
+//     console.log(a);
+// } while(a < 10)
+ 
+// function funcao(){
+//     console.log("tudo certo")
+// }
+
+// funcao();
+
+// function mensagem(primeiro,segundo) {
+//     console.log(primeiro,segundo)
+// }
+// mensagem("tudo certo", "jovem");
+
+// function funcao(){
+//     console.log("salve povo")
+// }
+
+// funcao();
+
+// var funcao = function (){
+//     console.log("Tudo funfando")
+// } 
+// funcao();
+
+// var funcao = () => {
+//     console.log("Sou uma arrow function")
+// }
+// funcao();
